d7f81e782660fe4e8c138cd2b8e08546ae11e900
Ricky Tang <tangricky001@gmail.com>
2025-06-06 09:08:56 -0400

    change it to vieta's formulas

--- mathletes-algebra-animation/src/scenes/example.tsx
+++ mathletes-algebra-animation/src/scenes/example.tsx
@@ -1,16 +1,62 @@
 import {Latex, makeScene2D} from '@motion-canvas/2d';
-import {createRef, waitFor} from '@motion-canvas/core';
+import {createRef, waitFor, chain} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
-  const tex = createRef<Latex>();
-  view.add(<Latex ref={tex} tex={['a^2', '+', '2a', '+', '1', '=', '0']} fill="white" />);
+  const equation = createRef<Latex>();
+  const sumFormula = createRef<Latex>();
+  const productFormula = createRef<Latex>();
+
+  // 1. Add quadratic equation with colored coefficients
+  view.add(
+    <Latex
+      ref={equation}
+      tex={[
+        '\\textcolor{red}{a}', 'x^2', '+',
+        '\\textcolor{blue}{b}', 'x', '+',
+        '\\textcolor{green}{c}', '=', '0',
+      ]}
+      fill="white"
+      y={-150}
+      fontSize={60}
+    />
+  );
 
   yield* waitFor(0.5);
-  yield* tex().tex(['(', 'a', '+', '1', ')^2', '=', '0'], 1);
 
-  yield* waitFor(0.5);
-  yield* tex().tex(['a', '+', '1', '=', '0'], 1);
+  // 2. Add Vieta's formulas below
 
-  yield* waitFor(0.5);
-  yield* tex().tex(['a', '=', '-1'], 1);
+  view.add(
+    <Latex
+      ref={sumFormula}
+      tex={[
+        'x_1', '+', 'x_2', '=', 
+        '-\\textcolor{blue}{b}', '/', '\\textcolor{red}{a}'
+      ]}
+      fill="white"
+      y={0}
+      fontSize={48}
+      opacity={0}
+    />
+  );
+
+  view.add(
+    <Latex
+      ref={productFormula}
+      tex={[
+        'x_1', 'x_2', '=', 
+        '\\textcolor{green}{c}', '/', '\\textcolor{red}{a}'
+      ]}
+      fill="white"
+      y={100}
+      fontSize={48}
+      opacity={0}
+    />
+  );
+
+  // 3. Animate formulas fading in
+  
+  yield* chain(
+    sumFormula().opacity(1, 1),
+    productFormula().opacity(1, 1),
+  );
 });
