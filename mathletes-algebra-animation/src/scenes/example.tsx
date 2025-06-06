591817366810b0498168c626772518a7259beae2
Ricky Tang <tangricky001@gmail.com>
2025-06-05 21:22:00 -0400

    simple quadratic equation animation

--- mathletes-algebra-animation/src/scenes/example.tsx
+++ mathletes-algebra-animation/src/scenes/example.tsx
@@ -1,21 +1,16 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {all, createRef} from '@motion-canvas/core';
+import {Latex, makeScene2D} from '@motion-canvas/2d';
+import {createRef, waitFor} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
-  const myCircle = createRef<Circle>();
+  const tex = createRef<Latex>();
+  view.add(<Latex ref={tex} tex={['a^2', '+', '2a', '+', '1', '=', '0']} fill="white" />);
 
-  view.add(
-    <Circle
-      ref={myCircle}
-      x={-300}
-      width={140}
-      height={140}
-      fill="#e13238"
-    />,
-  );
+  yield* waitFor(0.5);
+  yield* tex().tex(['(', 'a', '+', '1', ')^2', '=', '0'], 1);
 
-  yield* all(
-    myCircle().position.x(300, 1).to(-300, 1),
-    myCircle().fill('#e6a700', 1).to('#e13238', 1),
-  );
+  yield* waitFor(0.5);
+  yield* tex().tex(['a', '+', '1', '=', '0'], 1);
+
+  yield* waitFor(0.5);
+  yield* tex().tex(['a', '=', '-1'], 1);
 });
